0bc9dc4d618ed6e007e67b3dfb846c43ad2a7f23
Rayckaa <00001229453465sp@al.educacao.sp.gov.br>
2024-09-05 15:38:53 -0300

    raycka-aventura

---
+++ script.js
@@ -0,0 +1,15 @@
+const avanca = document.querySelectorAll(´.btn-proximo´)
+ 
+ 
+    avanca.forEach(button => {
+        button.addEventListener (´click´, function(
+            cont atual = document. querySelector (´.ativo´)
+            const proximoPasso= ´passo´ +this.getATTribute(´data-proximo´);
+ 
+            atual.classList. remove (´ativo´)
+            document.getElementById (proximoPasso). classList.add(´ativo´);
+ 
+        {
+           
+        })
+    }
